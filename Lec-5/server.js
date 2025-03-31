32460793f68ccb2f4ddd0c6e341a12853088db3b
Nitesh <nitesh.e21@nsut.ac.in>
2025-03-31 18:01:50 +0530

    BEE

--- Lec-5/server.js
+++ Lec-5/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended:true}))
+let usersData=[]
 app.get("/adduser",(req,res)=>{
     res.sendFile(__dirname+"/register.html");
 })
 app.post("/adduser",(req,res)=>{
     let {name,email,password} = req.body;
-    console.log(name,email,password)
+    let newUser={
+        name:name,
+        email:email,
+        password:password
+    }
+    usersData.push(newUser);
+    res.send(usersData);
+    // console.log(name,email,password)
+})
+app.get("/alluser",(req,res)=>{
+    res.send(usersData);
 })
 
 
